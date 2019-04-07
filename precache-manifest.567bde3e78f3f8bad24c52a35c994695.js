23be6807824e3d3f5b9758acffdab85b35da7c5a
Linda Lee <redtaq@hotmail.com>
2019-04-06 20:56:10 -0500

    Updates

---
+++ precache-manifest.567bde3e78f3f8bad24c52a35c994695.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae1a427b5c96524313b8",
+    "url": "/daughter-in-the-box/static/css/main.ea61aae8.chunk.css"
+  },
+  {
+    "revision": "ae1a427b5c96524313b8",
+    "url": "/daughter-in-the-box/static/js/main.fddda342.chunk.js"
+  },
+  {
+    "revision": "5d170975c542f1237b9e",
+    "url": "/daughter-in-the-box/static/js/runtime~main.7795cab1.js"
+  },
+  {
+    "revision": "871908c99a16d43f8891",
+    "url": "/daughter-in-the-box/static/css/2.db2249c0.chunk.css"
+  },
+  {
+    "revision": "871908c99a16d43f8891",
+    "url": "/daughter-in-the-box/static/js/2.5c043474.chunk.js"
+  },
+  {
+    "revision": "11b63f7f6be15ae4be7653578721dc50",
+    "url": "/daughter-in-the-box/static/media/gridBack.11b63f7f.jpg"
+  },
+  {
+    "revision": "7606befd2f8ff8a92424a77e08b51902",
+    "url": "/daughter-in-the-box/static/media/sky.7606befd.jpg"
+  },
+  {
+    "revision": "46b3b6060383d461babdc93305007618",
+    "url": "/daughter-in-the-box/static/media/wt034.46b3b606.ttf"
+  },
+  {
+    "revision": "951f3c4a7c3014403a075cb1fb3a801c",
+    "url": "/daughter-in-the-box/index.html"
+  }
+];
